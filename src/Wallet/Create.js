98458e470f0ac0ec40b20fefbfe21dd6dbb2a71c
Unknown <joshquin@gmail.com>
2017-10-21 14:28:39 +0400

    Create wallet for Android

--- src/Wallet/Create.js
+++ src/Wallet/Create.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Alert, StyleSheet, Text, View, TextInput } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import { Button, Card, ListItem } from 'react-native-elements';
+import bip39 from 'react-native-bip39';
 
 const hdKey = require('ethereumjs-wallet/hdkey');
-const bip39 = require('bip39');
 
 const MINIMUM_PASSWORD_LENGTH = 8;
 
@@ -17,9 +17,9 @@ export default class Create extends React.Component {
     this.createWallet = this.createWallet.bind(this);
   }
 
-  componentWillMount() {
-    let mnemonic = bip39.generateMnemonic();
-    this.setState({ mnemonic });
+  async componentWillMount() {
+    let mnemonic = await Create.generateMnemonic();
+    this.setState({mnemonic});
   }
 
   createWallet() {
@@ -35,9 +35,9 @@ export default class Create extends React.Component {
           <Text style={{ fontWeight: 'bold', color: 'red', textAlign: 'center'}}>
             These 12 words are the only way to restore your accounts. Save them somewhere safe and secret.
           </Text>
-          <Text style={{ padding: 20}}>{this.state.mnemonic}</Text>  
+          <Text style={{ padding: 20}}>{this.state.mnemonic}</Text>
         </Card>
-        <Button 
+        <Button
           onPress={this.createWallet}
           title="I've saved my words safely. Create wallet"
         />
@@ -46,4 +46,10 @@ export default class Create extends React.Component {
   }
 }
 
-
+Create.generateMnemonic = async () => {
+  try {
+    return await bip39.generateMnemonic(256) // default to 128
+  } catch(e) {
+    return false
+  }
+}
--- src/Wallet/Import.js
+++ src/Wallet/Import.js
@@ -1,14 +1,48 @@
 import React from 'react';
 import getWeb3 from '../utils/getWeb3'
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { Text, View, TextInput, Button, Modal } from 'react-native';
 import Web3 from 'web3';
 import { StackNavigator } from 'react-navigation';
 
 export default class Import extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mnemonic: '',
+      passwordPromptVisible: false
+    };
+  }
+
+  onChangeTextMnemonic(text) {
+    this.setState({mnemonic: text});
+  }
+
+  onPressImport() {
+    // Import Wallet
+    let mnemonic = this.state.mnemonic;
+    this.props.screenProps.setWallet(mnemonic);
+
+    // Navigate to Wallet screen
+    this.props.navigation.navigate('Main');
+  }
+
   render() {
     return (
-        <View>
-        <Text>Import Wallet!</Text>
+        <View style={{padding: 20}}>
+          <Text style={{fontWeight: 'bold'}}>Import Wallet</Text>
+          <Text>Input your 12 word mnemonic:</Text>
+          <TextInput
+            multiline={true}
+            numberOfLines={3}
+            editable={true}
+            value={this.state.mnemonic}
+            onChangeText={this.onChangeTextMnemonic.bind(this)}
+          />
+          <Button
+            onPress={this.onPressImport.bind(this)}
+            title="Import"
+          />
         </View>
     );
   }
